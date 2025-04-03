95ae986709d98e50b4b13224f2850b3f27486d83
Titouan Biget <71331020+TitouBiget@users.noreply.github.com>
2025-04-03 17:01:06 +0200

    Add files via upload

--- diff_dem_2.py
+++ diff_dem_2.py
@@ -25,32 +25,44 @@ ds = xr.open_dataset("stack_xdem.nc", engine="netcdf4")
 # ds = xr.open_dataset("stack_xdem_sent2.nc", engine="netcdf4")
 
 # ds['diff_elev'] = ds['elevation'] - ds['elevation_50m']
+slope50 = (ds.slope_horn_50m.values > 45) & ds.mask
+
 
 
 # %%
-def calculate_CWTMexHat(pathin, pathout = './cache_explore/', replace = False, lambdaa = 5):
+def Confusion_matrix(snow_obs_sample, snow_pred_sample, plot = True):
+    arr= snow_obs_sample.astype('int') - snow_pred_sample.astype('int') 
+    arr2 = snow_obs_sample.astype('int') + snow_pred_sample.astype('int') 
+    size = len(snow_obs_sample)* len(snow_obs_sample[0])
     
-    if not os.path.exists('./cache_explore/'):
-        os.makedirs('./cache_explore/')
+    arr_22 = sum(sum((arr2 == 0)))/size
+    arr_11 = sum(sum((arr2 == 2)))/size
     
-    f = os.path.join(pathout, pathin.split('.')[0].split('/')[-1]) + 'TC' + 'CWTMexHat_' + str(lambdaa) +'_m.tif'
-    if  not os.path.exists(f) or replace == True:
-        cwt = tc.CWTMexHat(Lambda=lambdaa)
-        Z, result = cwt.analyze(pathin)
-        cwt.export_result(f)
-    else:
-        print('Feature already processed')
-    with rasterio.open(f) as dataset:
-        Z=dataset.read(1)
-        
-    Z[Z == -9999] = np.nan
-    return Z
+    arr_21 = sum(sum((arr == 1)))/size
+    arr_12 = sum(sum((arr == -1)))/size
+    
+    
+    conf_matrix = np.array([[arr_11, arr_21] , [arr_12, arr_22]])
+    
+    if plot == True:
+        plt.figure()
+        plt.title(f'Total: {size}, prediction right: {arr_11 + arr_22}, prediction false: {arr_12 + arr_21}')
+        plt.imshow(conf_matrix)
+        plt.xticks([0,1], ['True', 'False'])
+        plt.yticks([0,1], ['True', 'False'])
+        plt.ylabel('Predicted')
+        plt.xlabel('Observed')
+        plt.text(0, 0 , s = f'{arr_11:.0%}', horizontalalignment='center', verticalalignment='center', fontsize = 15)
+        plt.text(0, 1 , s = f'{arr_12:.0%}', horizontalalignment='center', verticalalignment='center', fontsize = 15)
+        plt.text(1, 0 , s = f'{arr_21:.0%}', horizontalalignment='center', verticalalignment='center', fontsize = 15)
+        plt.text(1, 1 , s = f'{arr_22:.0%}', horizontalalignment='center', verticalalignment='center', fontsize = 15)
+    return conf_matrix
 
 
 def perf(obs, pred):
     arr = (obs == pred)
     size = len(arr)* len(arr[0])
-    return sum(sum(arr))/size
+    return (sum(sum(arr))/size)
 
 
 def x_diff(mat,  h):
@@ -69,61 +81,154 @@ ds['y_diff'] = (('y','x'), y_diff(ds.diff_elev.values, 1.5))
  
 th = 0.01
 
+def submatrix(mat, lim = (0,0,0,0)):
+    xmin, xmax, ymin, ymax = lim
+    return mat[xmin:xmax, ymin:ymax]
 
-
-
-slope50 = (ds.slope_horn_50m.values > 45) & ds.mask
 
 
 # %%
 
+x_value = (ds.x_diff.values) * (np.sin(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 for spot 7
+y_value = (ds.y_diff.values) * (np.cos(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 for spot 7
 
+mat2= ((x_value + y_value)< 1.4) & ((x_value + y_value)> -.9) & slope50
 
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2,3 , sharex = 1, sharey = 1)
-
-
-x_value = (ds.x_diff.values) * (np.sin(ds.aspect_horn.values*np.pi/180))
-y_value = (ds.y_diff.values) * (np.cos(ds.aspect_horn.values*np.pi/180))
-
-
-# mat1 = ((x_value**2 + y_value**2)**0.5 <.58) & slope50 #.30
+if True:
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2,3 , sharex = 1, sharey = 1)
     
-x_value = (ds.x_diff.values) * (np.sin(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 
-y_value = (ds.y_diff.values) * (np.cos(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 
+    
+    # x_value = (ds.x_diff.values) * (np.sin(ds.aspect_horn.values*np.pi/180))
+    # y_value = (ds.y_diff.values) * (np.cos(ds.aspect_horn.values*np.pi/180))
+    
+    
+    # mat1 = ((x_value**2 + y_value**2)**0.5 <.58) & slope50 #.30
+        
+    
+    
+    ax1.imshow(x_value, vmin = -.5, vmax = .5)
+    ax4.imshow(y_value, vmin = -.5, vmax = .5)
+    
+    
+    # mat2= ((x_value + y_value)< 0.112) & ((x_value + y_value)> -.58) & slope50 ######### top 1 for spot 7
+    
+    
+    ax2.imshow(ds.x_diff, vmin = -.5, vmax = .5)
+    # ax2.imshow(ds.snow_patches>1)
+    # ax5.imshow(mat2, vmin = -.5, vmax = .5)
+    ax5.imshow(ds.y_diff, vmin = -.5, vmax = .5)
+    
+    ax3.imshow((mat2).astype('int')  - (~slope50)) ######### top 1
+    
+    ax3.set_title('prediction') ######### top 1
+    
+    
+    ax6.imshow(ds.spot.isel(time = 1), cmap = 'grey')
+    ax6.set_title('PLE 2021_05_23')
+
+    
+    # ax6.imshow(ds.spot.isel(time = 0), cmap = 'managua')
+    
+    ax1.set_title('x_value')
+    ax4.set_title('y_value')
+    
+    ax2.set_title("x'")
+    ax5.set_title("y'")
+    
+    cursor = MultiCursor(fig.canvas, (ax1, ax2, ax3, ax4, ax5, ax6), color='cyan',lw=1, horizOn=True, vertOn=True)
+    plt.show()
+
+zone1 = (700,950,1250,1500) #[700:950, 1250:1500]
+zone2 = (780,975,780,1100) #[780:975, 780:1100]
+zone3 = (950,1060,1820,2220) #[950:1060, 1820:2220]
 
 
-ax1.imshow(x_value, vmin = -.5, vmax = .5)
-ax4.imshow(y_value, vmin = -.5, vmax = .5)
+c1 = Confusion_matrix( submatrix(mat2.values, zone1), submatrix(ds.snow_patches.values>0, zone1), 1)
+p1 = perf( submatrix(mat2.values, zone1), submatrix(ds.snow_patches.values>0, zone1))
+rd1 = perf( submatrix((np.random.rand(2250,2250) > 0.5).astype(int), zone1), submatrix(ds.snow_patches.values>0, zone1))
+one1 = perf( submatrix(np.ones((2250,2250)), zone1), submatrix(ds.snow_patches.values>0, zone1))
+zero1 = perf( submatrix(np.zeros((2250,2250)), zone1), submatrix(ds.snow_patches.values>0, zone1))
+
+c2 = Confusion_matrix( submatrix(mat2.values, zone2), submatrix(ds.snow_patches.values>0, zone2), 1)
+p2 = perf( submatrix(mat2.values, zone2), submatrix(ds.snow_patches.values>0, zone2))
+rd2 = perf( submatrix(((np.random.rand(2195,2320) > 0.5).astype(int)), zone2), submatrix(ds.snow_patches.values>0, zone2))
+one2 = perf( submatrix(np.ones((2250,2250)), zone2), submatrix(ds.snow_patches.values>0, zone2))
+zero2 = perf( submatrix(np.zeros((2250,2250)), zone2), submatrix(ds.snow_patches.values>0, zone2))
+
+c3 = Confusion_matrix( submatrix(mat2.values, zone3), submatrix(ds.snow_patches.values>0, zone3), 1)
+p3 = perf( submatrix(mat2.values, zone3), submatrix(ds.snow_patches.values>0, zone3))
+rd3 = perf( submatrix(((np.random.rand(3200,3000) > 0.5).astype(int)), zone3), submatrix(ds.snow_patches.values>0, zone3))
+one3 = perf( submatrix(np.ones((3200,3200)), zone3), submatrix(ds.snow_patches.values>0, zone3))
+zero3 =  perf( submatrix(np.zeros((3200,3200)), zone3), submatrix(ds.snow_patches.values>0, zone3))
+
+# print('c1', c1[0,0], c1[1,1], ' rd1', rd1[0,0], rd1[1,1])
+# print('c2', c2[0,0], c2[1,1], ' rd2', rd2[0,0], rd2[1,1])
+# print('c3', c3[0,0], c3[1,1], ' rd3', rd3[0,0], rd3[1,1])
+
+avg1 = (c1[0,1] + c2[0,1] + c3[0,1])/3
+avg2 = (c1[1,0] + c2[1,0] + c3[1,0])/3
+
+print(avg1 + avg2)
+
+print('perf ratio:')
+print("Zone 1: p1 ", p1, " rd1 ", rd1, " one1 ", one1, " zero1 ", zero1)
+print("Zone 2: p2 ", p2, " rd2 ", rd2, " one1 ", one2, " zero2 ", zero2)
+print("Zone 3: p3 ", p3, " rd3 ", rd3, " one3 ", one3, " zero3 ", zero3)
+
+# Confusion_matrix( np.ones((195,320)), (ds.snow_patches.values>0)[780:975, 780:1100])
+
+# plt.imshow
+
+# %%
 
 
-mat2= ((x_value + y_value)< 0.112) & ((x_value + y_value)> -.58) & slope50
+from tqdm import tqdm
 
-# ax2.imshow(mat1, vmin = -.5, vmax = .5)
-ax2.imshow(ds.snow_patches>1)
-# ax5.imshow(mat2, vmin = -.5, vmax = .5)
-ax5.imshow(ds.aspect_horn_50m)
-
-ax3.imshow((mat2).astype('int')  - (~slope50)) ######### top 1
-
-ax3.set_title('prediction') ######### top 1
+list_score = []
 
 
-ax6.imshow(ds.spot.isel(time = 0), cmap = 'grey')
-ax6.set_title('SPOT7')
-# ax6.imshow(ds.spot.isel(time = 0), cmap = 'managua')
 
-ax1.set_title('x_value')
-ax4.set_title('y_value')
+x_value = (ds.x_diff.values) * (np.sin(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 for spot 7
+y_value = (ds.y_diff.values) * (np.cos(ds.aspect_horn.values*np.pi/180)**2) ######### top 1 for spot 7
 
-ax2.set_title('snow w/ skimage')
-ax5.set_title('aspect')
+zone1 = (700,950,1250,1500) #[700:950, 1250:1500]
+zone2 = (780,975,780,1100) #[780:975, 780:1100]
+zone3 = (950,1060,1820,2220) #[950:1060, 1820:2220]
 
-cursor = MultiCursor(fig.canvas, (ax1, ax2, ax3, ax4, ax5, ax6), color='cyan',lw=1, horizOn=True, vertOn=True)
-plt.show()
+l = len(np.arange(0, 3, 0.1))
+heatmap = np.zeros((l,l))
 
-# plt.imshow(ds.snow_patches.values>0, alpha= 0.4, cmap = 'grey' )
+for i, j in enumerate(np.arange(0, 3, 0.1)):
+    for m, k in enumerate(np.arange(0, 3, 0.1)):
 
-# ds['prediction'] = (['y','x'], mat2)
+
+        mat2= ((x_value + y_value)< j) & ((x_value + y_value)> -k) & slope50
+
+        c1 = Confusion_matrix( submatrix(mat2.values, zone1), submatrix(ds.snow_patches.values>0, zone1), False)
+        rd1 = Confusion_matrix( submatrix((np.random.rand(2250,2250) > 0.5).astype(int), zone1), submatrix(ds.snow_patches.values>0, zone1), False)
+        
+        c2 = Confusion_matrix( submatrix(mat2.values, zone2), submatrix(ds.snow_patches.values>0, zone2), False)
+        rd2 = Confusion_matrix( submatrix(((np.random.rand(2195,2320) > 0.5).astype(int)), zone2), submatrix(ds.snow_patches.values>0, zone2), False)
+        
+        c3 = Confusion_matrix( submatrix(mat2.values, zone3), submatrix(ds.snow_patches.values>0, zone3), False)
+        rd3 = Confusion_matrix( submatrix(((np.random.rand(3200,3000) > 0.5).astype(int)), zone3), submatrix(ds.snow_patches.values>0, zone3), False)
+        
+        
+        # print('c1', c1[0,0], c1[1,1], ' rd1', rd1[0,0], rd1[1,1])
+        # print('c2', c2[0,0], c2[1,1], ' rd2', rd2[0,0], rd2[1,1])
+        # print('c3', c3[0,0], c3[1,1], ' rd3', rd3[0,0], rd3[1,1])
+        
+        avg1 = (c1[0,1] + c2[0,1] + c3[0,1])/3
+        avg2 = (c1[1,0] + c2[1,0] + c3[1,0])/3
+        
+        heatmap[i,m] = avg1 + avg2
+    print(i)
+
+plt.imshow(heatmap, vmax = 0.28)
+# plt.xticks(range(l), labels= np.arange(0, 3, 0.1))
+# plt.yticks(range(l), labels= np.arange(0, 3, 0.1))
+           
+
 # %%
 snow_obs_sample = mat2[700:950, 1250:1500]
 snow_pred_sample = (ds.snow_patches.values>0)[700:950, 1250:1500]
@@ -135,15 +240,8 @@ arr = (snow_obs_sample == snow_pred_sample)
 h1 = arr * ds.slope_horn_diff.values[700:950, 1250:1500]
 h2 = ~arr * ds.slope_horn_diff.values[700:950, 1250:1500]
 
-l1 = arr * ds.slope_horn.values[700:950, 1250:1500]
-l2 = ~arr * ds.slope_horn.values[700:950, 1250:1500]
-
-
 h11 = h1.values.reshape(1, -1)[0]
 h12 = h2.values.reshape(1, -1)[0]
-
-l11 = l1.values.reshape(1, -1)[0]
-l12 = l2.values.reshape(1, -1)[0]
 
 plt.figure()
 plt.hist(h11, bins = 50)
@@ -151,6 +249,17 @@ plt.title('aspect prediction true')
 plt.figure()
 plt.hist(h12, bins = 50)
 plt.title('aspect prediction false')
+# %% confusion_matrix
+
+Confusion_matrix( mat2[700:950, 1250:1500], (ds.snow_patches.values>0)[700:950, 1250:1500])
+
+# %%
+
+l1 = arr * ds.slope_horn.values[700:950, 1250:1500]
+l2 = ~arr * ds.slope_horn.values[700:950, 1250:1500]
+
+l11 = l1.values.reshape(1, -1)[0]
+l12 = l2.values.reshape(1, -1)[0]
 
 plt.figure()
 plt.hist(l11, bins = 50)
@@ -232,7 +341,30 @@ per = perf(snow_obs_sample, snow_pred_sample)
  
 print(per)
 
+# %%
+import pywt
 
+# Load image
+original = ds.diff_elev.values
+
+# Wavelet transform of image, and plot approximation and details
+titles = ['Approximation', ' Horizontal detail',
+          'Vertical detail', 'Diagonal detail']
+coeffs2 = pywt.dwt2(original, 'bior1.1')
+LL, (LH, HL, HH) = coeffs2
+fig = plt.figure(figsize=(12, 3))
+ax1 = fig.add_subplot(2, 4 , 1)  # equivalent but more general
+for i, a in enumerate([LL, LH, HL, HH]):
+    ax = fig.add_subplot(2, 4, i + 1, sharex = ax1, sharey = ax1)
+    ax.imshow(a<0, interpolation="nearest", cmap=plt.cm.gray)
+    ax.set_title(titles[i], fontsize=10)
+    ax.set_xticks([])
+    ax.set_yticks([])
+ax = fig.add_subplot(2, 4 , 5, sharex = ax1, sharey = ax1) 
+ax.imshow(ds.spot.isel(time=0).values[::2, ::2])
+
+fig.tight_layout()
+plt.show()
 # %%
 from tqdm import tqdm
 
